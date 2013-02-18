b479e6f35ebc7b6d2e7d9152f9a80b405344e766
Nathan Cartwright <fshost@yahoo.com>
2013-02-18 00:35:43 -0600

    initial local commit

---
+++ require-ejs.js
@@ -0,0 +1,32 @@
+﻿var fs = require('fs');
+
+
+var path = require('path');
+
+var ejs = require(path.resolve(path.join(__dirname, 'lib', 'ejs')));
+
+function removeBOM(text) {
+    if (text.indexOf('\uFEFF') === 0) {
+        text = text.substring(1, text.length);
+    }
+    return text;
+}
+
+require.extensions['.ejs'] = function (module) {
+
+    var filename = module.filename;
+
+    var options = { filename: filename, client: true };
+
+    if (process.env.NODE_ENV && process.env.NODE_ENV.toString().toLowerCase() === 'development') {
+        options.compileDebug = true;
+    }
+
+    var template = removeBOM(fs.readFileSync(filename).toString());
+
+    var fn = ejs.compile(template, options);
+    
+    var src = ['module.exports = ', fn.toString(), ';'];
+    module._compile(src.join(''), filename);
+
+};
---
+++ test/test.js
@@ -0,0 +1,10 @@
+﻿require(__dirname + '/../require-ejs');
+
+var assert = require('assert');
+
+var page = require(__dirname + '/views/layout');
+var expected = '<b>test-page</b><p>test page text</p>';
+var result = page({ title: 'test-page', text: 'test page text' });
+
+assert.equal(result, expected);
+
